2bdb24a2111e1fc974a3bc43225f39d362f8cb23
Charliekofg <charliekofg@hotmail.com>
2025-10-07 04:15:02 +0000

    tbd:fromDollarToYen and fromYenToPound

---
+++ .learn/resets/07-running-the-test/app.js
@@ -0,0 +1,13 @@
+//This is my function that sums two numbers
+
+const sum = (a,b) => {
+    return a + b
+}
+
+// Just a console log for ourselves
+console.log(sum(7,3));
+
+
+//Export of the funcion
+
+module.exports = { sum };
---
+++ app.js
@@ -0,0 +1,43 @@
+// We declare the function with the exact name "fromEuroToDollar"
+const fromEuroToDollar = function(valueInEuro) {
+    // Convert the given valueInEuro to dollars
+    let valueInDollar = valueInEuro * 1.07;
+    // return the dollar value
+    return valueInDollar;
+}
+
+const fromDollarToYen = function(valueInDollar) {
+    let valueInYen = valueInDollar * 146.26;
+    return valueInYen;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//This is my function that sums two numbers
+
+const sum = (a,b) => {
+    return a + b
+}
+
+// Just a console log for ourselves
+console.log(sum(7,3));
+
+
+//Export of the funcion
+
+module.exports = { sum, fromEuroToDollar, fromDollarToYen };
+
+
